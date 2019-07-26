778f09bb458e80ccb0b08896567e0e535d185ca7
Junior Paz Formoso <junior.presagios@gmail.com>
2019-07-26 12:38:12 -0400

    Added an app to geo information

---
+++ apps/icona_geo/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from apps.icona_geo.models import Country, City, Cap
+
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Cap)
---
+++ apps/icona_geo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IconaGeoConfig(AppConfig):
+    name = 'icona_geo'
